33ed5ba268524c284148ea516bdeb8019c4bad33
stop-sine <57299283+stop-sine@users.noreply.github.com>
2025-10-25 19:03:25 -0500

    Initial commit for new patcher

---
+++ OblivionInteractionIconsPatcher/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using System.Threading.Tasks;
+using Mutagen.Bethesda.FormKeys.SkyrimSE;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Records;
+
+namespace OblivionInteractionIconsPatcher
+{
+    class Progam
+    {
+
+    }
+}
